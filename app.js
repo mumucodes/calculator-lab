3975574a67fc808649243be619606d1ba86670e0
Hayden Buell <kraterhaydenbuell@gmail.com>
2021-02-11 11:43:14 -0800

    calculator lab

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
-// import functions and grab DOM elements
+// igrab DOM elements
 
 // initialize state
 
-// set event listeners to update state and DOM
+// set event listeners to update on each button, do math and update DOM
+
+
+const additionNumberOne = document.getElementById('value-x');
+const additionNumberTwo = document.getElementById('value-y');
+const additionButton = document.getElementById('calculate-addition');
+const additionDisplay = document.getElementById('display-addition');
+
+additionButton.addEventListener('click', () => {
+    const sum = Number(additionNumberOne.value) + Number(additionNumberTwo.value);
+    additionDisplay.textContent = sum;
+
+});
+
+
+const subtractionNumberOne = document.getElementById('subtraction-x');
+const subtractionNumberTwo = document.getElementById('subtraction-y');
+const subtractionButton = document.getElementById('calculate-subtraction');
+const subtractionDisplay = document.getElementById('display-subtraction');
+
+subtractionButton.addEventListener('click', () => {
+    const sum = Number(subtractionNumberOne.value) + Number(subtractionNumberTwo.value);
+    subtractionDisplay.textContent = sum;
+
+});
+
+const multiplyNumberOne = document.getElementById('multiply-x');
+const multiplyNumberTwo = document.getElementById('multiply-y');
+const multiplyButton = document.getElementById('calculate-multiplication');
+const multiplicationDisplay = document.getElementById('display-multiplication');
+
+multiplyButton.addEventListener('click', () => {
+    const sum = Number(multiplyNumberOne.value) * Number(multiplyNumberTwo.value);
+    multiplicationDisplay.textContent = sum;
+
+});
+
+const divideNumberOne = document.getElementById('divide-x');
+const divideNumberTwo = document.getElementById('divide-y');
+const divisionButton = document.getElementById('calculate-division');
+const divisionDisplay = document.getElementById('display-division');
+
+divisionButton.addEventListener('click', () => {
+    const sum = Number(divideNumberOne.value) / Number(divideNumberTwo.value);
+    divisionDisplay.textContent = sum;
+
+});
